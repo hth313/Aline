67963a92de0e887351cfd0295bca5d5636c2944a
Håkan Thörngren <hth313@gmail.com>
2021-02-14 14:43:43 -0800

    Add a generator for sine "offline"

---
+++ example/sineGenerate/main.c
@@ -0,0 +1,44 @@
+#include "stdio.h"
+#include "math.h"
+
+union xx {
+  int ii;
+  float ff;
+};
+
+int main () {
+  for (unsigned i = 0; i <= 255; i++) {
+    double result = sin((double) i * 90.0 / 64.0 * (M_PI / 180));
+    union xx conv;
+    conv.ff = result;
+    int sign = conv.ii < 0;
+    conv.ii &= 0x7fffffff;
+    int frac = (conv.ii & 0x7fffff) | 0x800000;
+    int exp = (conv.ii >> 23) - 127;
+
+    while (exp) {
+      frac >>= 1;
+      exp++;
+    }
+
+    if (   (frac & 0xc000) == 0x4000 && (frac & 0x3000) != 0
+	|| (frac & 0xc000) == 0xc000) {
+      frac += 0x8000;
+      if (frac > 0xffffff) {
+	frac >>= 1;
+      }
+    }
+    frac >>= 15;
+    if (sign) {
+      frac = (-frac) & 0xffff;
+    }
+
+    if (conv.ii == 0) {
+      frac = 0;
+      exp = 0;
+    }
+//    printf("0x%04x,\n", frac);
+    printf("0x%02x,\n", frac >> 2);
+
+  }
+}
